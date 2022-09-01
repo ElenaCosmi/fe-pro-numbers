65f9a2437797c427722023d15e497e082a77c536
Elena Kuzmichova <elenakuzmichova@cosmi-air.home>
2022-09-01 15:43:22 +0100

    homework7 is done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 /**
  * Принимает 4 аргумента, в первых двух мы должны найти минимальное число,
  * во третьем и четвертом максимальное и умножить эти два числа
- * @param {number} minFirst
- * @param {number} minSecond
- * @param {number} maxFirst
- * @param {number} maxSecond
+ * @param {2} minFirst
+ * @param {5} minSecond
+ * @param {8} maxFirst
+ * @param {10} maxSecond
  * @returns {number}
  */
-export const minMaxMultiply = (minFirst, minSecond, maxFirst, maxSecond) => {};
+
+export const minMaxMultiply = (minFirst, minSecond, maxFirst, maxSecond) => {
+  return (Math.min(minFirst, minSecond)) * (Math.max(maxFirst, maxSecond));
+};
 
 /**
  * Функция принимает один числовой аргумент, это радиус круга. Функция должна
@@ -16,7 +19,9 @@ export const minMaxMultiply = (minFirst, minSecond, maxFirst, maxSecond) => {};
  * @param {number} radius
  * @returns {number}
  */
-export const getSquareOfCircle = (radius) => {};
+export const getSquareOfCircle = (radius) => {
+  return Math.PI * radius ** 2;
+};
 
 /**
  * Функция принимает число и точность к которой нужно округлить
@@ -26,7 +31,9 @@ export const getSquareOfCircle = (radius) => {};
  * @param {number} accuracy
  * @returns {number}
  */
-export const getFixedValue = (number, accuracy) => {};
+export const getFixedValue = (number, accuracy) => {
+  return Math.round(number * 1000) / 1000
+};
 
 /**
  * Если целая часть числа четная, то срабатывает Math.floor для этого числа
@@ -34,7 +41,14 @@ export const getFixedValue = (number, accuracy) => {};
  * @param {number} number
  * @returns {number}
  */
-export const getFlooroCeil = (number) => {};
+export const getFlooroCeil = (number) => {
+  let x = Math.round(number)
+  if (x % 2 === 0) {
+    return Math.floor(number)
+  } else {
+    return Math.ceil(number)
+  }
+};
 
 /**
  * Мы передаем два числа, от какого и до какого числа и функция должна
@@ -44,4 +58,6 @@ export const getFlooroCeil = (number) => {};
  * @param {number} to
  * @returns {number}
  */
-export const randomizer = (from, to) => {};
+export const randomizer = (from, to) => {
+  return Math.round(Math.random() * (to - from) + from);
+};
